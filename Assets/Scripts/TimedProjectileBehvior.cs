7f6727200889006604686b09263e137ece6a7286
glennacgreer <34104409+glennacgreer@users.noreply.github.com>
2020-01-21 12:38:01 -0500

    Organization and Projectile Spawner

---
+++ Assets/Scripts/TimedProjectileBehvior.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedProjectileBehvior : MonoBehaviour
+{
+    public float force;
+    public Rigidbody rb;
+    public float lifetime;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb.AddForce(Vector3.forward * force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator Lifetime()
+    {
+        yield return new WaitForSeconds(lifetime);
+
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/TimedProjectiles.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimedProjectiles : MonoBehaviour
+{
+    public GameObject projectile;
+    public float delay;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(FiringSequence());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private IEnumerator FiringSequence()
+    {
+        yield return new WaitForSeconds(delay);
+
+        Instantiate(projectile);
+        StartCoroutine(FiringSequence());
+    }
+}
