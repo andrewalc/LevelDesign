61290ecedf9a2154848c5248cef1856f1eb83ef5
Andrew Alcala <andrewalc98@gmail.com>
2020-02-06 16:28:10 -0500

    Animation when boss is hit

---
+++ Assets/Scripts/Week4/Crystal.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crystal : MonoBehaviour
+{
+
+    private Rigidbody rb;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.transform.CompareTag("Laser"))
+        {
+            print(other.transform.tag);
+            rb.useGravity = true;
+        }
+    }
+}
---
+++ Assets/Scripts/Week4/Week4Boss.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Week4Boss : MonoBehaviour
+{
+
+    public UnityEvent onCrystalEnter;
+
+    private List<GameObject> collidedCrystals;
+    // Start is called before the first frame update
+    void Start()
+    {
+     collidedCrystals = new List<GameObject>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Crystal") && !collidedCrystals.Contains(other.gameObject))
+        {
+            collidedCrystals.Add(other.gameObject);
+            onCrystalEnter.Invoke();
+        }
+    }
+}
