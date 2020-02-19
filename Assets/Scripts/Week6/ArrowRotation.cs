f6db499b8d38cea10599ca689895e7ce0bfa9dec
andrewalc <andrewalc98@gmail.com>
2020-02-19 03:03:18 -0500

    Added DOTween. Charge mechanic functional

---
+++ Assets/Scripts/Week6/ArrowRotation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using DG.Tweening.Core;
+using DG.Tweening.Plugins.Options;
+using UnityEngine;
+
+public class ArrowRotation : MonoBehaviour
+{
+    public float bobbingRate;
+    private bool bobbing = false;
+
+    private void Start()
+    {
+        transform.DOLocalRotate(new Vector3(0, 360.0f, 0), 2f, RotateMode.FastBeyond360)
+            .SetRelative(true)
+            .SetEase(Ease.Linear)
+            .SetLoops(-1, LoopType.Incremental);
+        transform.DOLocalMove(Vector3.down * 2f, bobbingRate)
+            .SetRelative(true)
+            .SetLoops(-1, LoopType.Yoyo);
+    }
+}
---
+++ Assets/Scripts/Week6/WaterDash.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UIElements;
+
+public class WaterDash : MonoBehaviour
+{
+    public float chargeRate;
+    public float travelTime;
+    public GameObject targetPrefab;
+    private GameObject _instantiatedPrefab;
+    private float _chargeDistance;
+
+    private void Start()
+    {
+        _chargeDistance = 2;
+        _instantiatedPrefab = null;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            _chargeDistance += Time.deltaTime * chargeRate;
+            if (_instantiatedPrefab == null)
+            {
+                _instantiatedPrefab = Instantiate(targetPrefab, GetDashDestination(), Quaternion.identity);
+            }
+            else
+            {
+                _instantiatedPrefab.transform.position = GetDashDestination();
+            }
+        }
+        if (Input.GetMouseButtonUp(0))
+        {
+            Destroy(_instantiatedPrefab);
+            _instantiatedPrefab = null;
+            Dash();
+        }
+    }
+
+    private Vector3  GetDashDestination()
+    {
+        return transform.position + transform.forward.normalized * _chargeDistance;
+    }
+
+    private void Dash()
+    {
+        transform.DOMove(GetDashDestination(), travelTime);
+        _chargeDistance = 2;
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(GetDashDestination(), travelTime);
+    }
+}
