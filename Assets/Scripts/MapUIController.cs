e72fca607ac143ef6e36edbd376d057607ae05d4
glennacgreer <34104409+glennacgreer@users.noreply.github.com>
2020-02-26 23:35:18 -0500

    Map

---
+++ Assets/Scripts/MapUIController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapUIController : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject map;
+    private float startingTimeScale;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        map.SetActive(false);
+        startingTimeScale = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            map.SetActive(true);
+            Time.timeScale = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Tab))
+        {
+            map.SetActive(false);
+            Time.timeScale = startingTimeScale;
+        }
+    }
+}
